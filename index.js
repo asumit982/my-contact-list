1d4701da3132f13282d393abb081e9edb70224c9
sumittricon <sumit.anshul@triconinfotech.com>
2022-12-22 17:07:39 +0530

    Created the contact list

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const path = require('path');
 
 const app = express();
 
+var contactList = [
+    {
+        name : "Spider Man",
+        phone : 587634863
+    },
+    {
+        name : "Iron Man",
+        phone : 547894383
+    },
+    {
+        name : "Batman",
+        phone : 3245263
+    }
+]
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
 app.get('/',function(req,res){
     return res.render('home', 
-    {title: "My Contact List"});
+    {title: "My Contact List",
+    contact_list : contactList
+});
 })
 
 app.listen(port, function(err){
